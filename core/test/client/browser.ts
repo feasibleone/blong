bfb0473de4ca61768bcc3f464d8d38e715f9cba0
Kalin Krustev <kalin.krustev@gmail.com>
2024-09-30 06:52:24 +0000

    chore: build

--- core/test/client/browser.ts
+++
@@ -1,15 +0,0 @@
-import unchanged from '@feasibleone/blong';import {realm} from '@feasibleone/blong';
-
-export default realm(blong => ({
-    url: import.meta.url,
-    validation: blong.type.Object({}),
-    children: ['./test'],
-    config: {
-        default: {},
-        dev: {},
-        microservice: {},
-        integration: {
-            test: true,
-        },
-    },
-}));
