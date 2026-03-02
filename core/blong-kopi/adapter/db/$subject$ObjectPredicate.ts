31e4a66ba2ffab92603a2a9a5dd2f2c0d7c2756b
Kalin Krustev <kalin.krustev@gmail.com>
2026-03-02 16:03:46 +0000

    feat: add Chromatic integration and new CI commands for testing

--- core/blong-kopi/adapter/db/$subject$ObjectPredicate.ts
+++ core/blong-kopi/adapter/db/$subject$ObjectPredicate.ts
@@ -3,8 +3,8 @@ import {type IMeta, handler} from '@feasibleone/blong';
 export default handler(
     proxy =>
         async function $subject$ObjectPredicate(params: {id: unknown}, $meta: IMeta) {
-            return await this.config.context.queryBuilder
-                .from('$object')
+            return await this.config?.context?.queryBuilder
+                ?.from('$object')
                 .where(params)
                 .select()
                 .first();
