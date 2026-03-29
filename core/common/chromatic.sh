35b73dcbe77cfdd78c2b55d5c3568e048143b39b
Kalin Krustev <kalin.krustev@gmail.com>
2026-03-29 01:12:47 +0000

    fix: build

--- core/common/chromatic.sh
+++ core/common/chromatic.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-if [[ "$CHROMATIC_PROJECT_TOKEN" == *$PWD* ]]; then
-  CHROMATIC_PROJECT_TOKEN=$(echo "$CHROMATIC_PROJECT_TOKEN" | grep "^$PWD=" | sed "s/^$PWD=//")
+if [[ "$CHROMATIC_PROJECT_TOKEN" == *$(basename "$PWD")* ]]; then
+  CHROMATIC_PROJECT_TOKEN=$(echo "$CHROMATIC_PROJECT_TOKEN" | grep "^$(basename "$PWD")=" | sed "s/^$(basename "$PWD")=//")
 fi
 
 chromatic --exit-zero-on-changes
