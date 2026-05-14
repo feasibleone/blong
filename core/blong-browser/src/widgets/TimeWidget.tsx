925c18574a1b159917294f591ad864e46339d407
Kalin Krustev <kalin.krustev@gmail.com>
2026-05-14 07:34:22 +0000

    fix: time widget remove date and leave only time

--- core/blong-browser/src/widgets/TimeWidget.tsx
+++ core/blong-browser/src/widgets/TimeWidget.tsx
@@ -1,6 +1,19 @@
+import React from 'react';
 import {Calendar} from '../primereact/index.js';
 
 import type {IWidgetProps} from '@feasibleone/blong';
+
+const timeIn = (d: string | Date) => {
+    const dd = new Date(d);
+    const properDate = new Date(
+        new Date(d).getTime() + new Date(d).getTimezoneOffset() * 60 * 1000,
+    );
+    const timezoneDiff = properDate.getTimezoneOffset() - dd.getTimezoneOffset();
+    if (!timezoneDiff) return properDate;
+    return new Date(properDate.getTime() + timezoneDiff * 60 * 1000);
+};
+
+const timeOut = (d: Date) => new Date(d.setFullYear(1970, 0, 1));
 
 export function TimeWidget({
     id,
@@ -12,12 +25,21 @@ export function TimeWidget({
     readOnly,
     disabled,
 }: IWidgetProps) {
-    const dateValue = value instanceof Date ? value : value ? new Date(value as string) : null;
+    const dateValue = value instanceof Date ? value : value ? timeIn(value as string) : null;
+
+    const handleChange = React.useMemo(
+        () => (event: {value: Date | null | undefined | string | Date[]}) => {
+            if (event.value instanceof Date) event.value = timeOut(event.value);
+            onChange(event.value);
+        },
+        [onChange],
+    );
+
     return (
         <Calendar
             inputId={id ?? name}
             value={dateValue}
-            onChange={e => onChange(e.value ?? null)}
+            onChange={handleChange}
             onHide={onBlur}
             readOnlyInput={readOnly}
             disabled={disabled}
