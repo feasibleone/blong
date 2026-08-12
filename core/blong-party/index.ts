76ee67ef0a38a47eb12fbccdee8be435ae074a46
Kalin Krustev <kalin.krustev@gmail.com>
2026-08-12 20:06:32 +0000

    fix: blong-party test

--- core/blong-party/index.ts
+++ core/blong-party/index.ts
@@ -35,6 +35,7 @@ export default server(() => ({
                 expectedErrors: true,
             },
             systemDebug: {enabled: true},
+            srv: {},
             core: {},
             access: {},
             party: {},
